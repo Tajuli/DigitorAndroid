d00eeb04f62f8fae12fbdc17e6441f99faa75f05
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-22 15:17:35 +0600

    Switch app to GPU-first zoomable timeline screen

--- app/src/main/java/com/tajuli/digitorandroid/MainActivity.kt
+++ app/src/main/java/com/tajuli/digitorandroid/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.tajuli.digitorandroid.ui.editor.DigitorEditorScreen
+import com.tajuli.digitorandroid.ui.editor.DigitorEditorScreenV2
 import com.tajuli.digitorandroid.ui.theme.DigitorTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DigitorTheme {
-                DigitorEditorScreen()
+                DigitorEditorScreenV2()
             }
         }
     }
