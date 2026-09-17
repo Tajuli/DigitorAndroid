458835b6f4c59a975420860b0f434e9d5f6ec932
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-09-17 09:20:05 +0600

    refactor(editor): use canonical view model in activity

--- app/src/main/java/com/tajuli/digitorandroid/MainActivity.kt
+++ app/src/main/java/com/tajuli/digitorandroid/MainActivity.kt
@@ -33,7 +33,7 @@ import com.tajuli.digitorandroid.editor.model.TimelineProject
 import com.tajuli.digitorandroid.editor.processing.CutoutAnalysisPowerGuardV48
 import com.tajuli.digitorandroid.ui.editor.ActiveEditorVmRegistryV14
 import com.tajuli.digitorandroid.ui.editor.DigitorEditorScreen
-import com.tajuli.digitorandroid.ui.editor.EditorViewModelV4
+import com.tajuli.digitorandroid.ui.editor.EditorViewModel
 import com.tajuli.digitorandroid.ui.home.DigitorHomeScreen
 import com.tajuli.digitorandroid.ui.theme.DigitorTheme
 import kotlinx.coroutines.Dispatchers
@@ -147,7 +147,7 @@ class MainActivity : ComponentActivity() {
 
                 when (destination) {
                     DESTINATION_EDITOR -> {
-                        val editorVm: EditorViewModelV4 = viewModel(key = "editor-session-$editorSession")
+                        val editorVm: EditorViewModel = viewModel(key = "editor-session-$editorSession")
                         ActiveEditorVmRegistryV14.bind(editorVm)
                         val editorState by editorVm.state.collectAsState()
                         latestEditorProject = editorState.project
