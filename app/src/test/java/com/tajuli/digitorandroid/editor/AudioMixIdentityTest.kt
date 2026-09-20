7bdf224576ad826de672e9848ea8050aa5038790
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-09-20 15:37:49 +0600

    Test adaptive noise floor and smoothing behavior

--- app/src/test/java/com/tajuli/digitorandroid/editor/AudioMixIdentityTest.kt
+++ app/src/test/java/com/tajuli/digitorandroid/editor/AudioMixIdentityTest.kt
@@ -1,7 +1,7 @@
 package com.tajuli.digitorandroid.editor
 
+import com.tajuli.digitorandroid.editor.model.AdaptiveNoiseReducer
 import com.tajuli.digitorandroid.editor.model.AudioMix
-import com.tajuli.digitorandroid.editor.model.audioNoiseReductionGain
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -16,9 +16,36 @@ class AudioMixIdentityTest {
     }
 
     @Test
-    fun noiseReductionLeavesLoudSpeechAndSuppressesLowLevelNoise() {
-        assertEquals(1f, audioNoiseReductionGain(.5f, 1f), 0.0001f)
-        val lowNoiseGain = audioNoiseReductionGain(.005f, 1f)
-        assert(lowNoiseGain < .2f)
+    fun adaptiveNoiseReductionSuppressesSteadyNoiseAndOpensForSpeech() {
+        val reducer = AdaptiveNoiseReducer(1f)
+        var gain = 1f
+
+        // About one second of low-level room noise should settle well below unity.
+        repeat(48_000) {
+            gain = reducer.processFrame(.006f, 48_000)
+        }
+        assert(gain < .35f)
+
+        // Speech should open the gate quickly without a hard discontinuity.
+        val beforeSpeech = gain
+        repeat(480) {
+            gain = reducer.processFrame(.45f, 48_000)
+        }
+        assert(gain > beforeSpeech)
+        assert(gain > .85f)
+
+        // Release should be gradual rather than snapping back to the noise floor.
+        val firstQuietFrame = reducer.processFrame(.006f, 48_000)
+        assert(firstQuietFrame > .75f)
+    }
+
+    @Test
+    fun adaptiveNoiseFloorCanTrackSustainedBackground() {
+        val reducer = AdaptiveNoiseReducer(.75f)
+        val initialFloor = reducer.currentNoiseFloorForTest()
+        repeat(96_000) {
+            reducer.processFrame(.015f, 48_000)
+        }
+        assert(reducer.currentNoiseFloorForTest() > initialFloor)
     }
 }
