3c7ec10b0bec309319afbf6655e6a518b204fbbc
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-09-20 14:48:01 +0600

    Test basic audio noise reduction model

--- app/src/test/java/com/tajuli/digitorandroid/editor/AudioMixIdentityTest.kt
+++ app/src/test/java/com/tajuli/digitorandroid/editor/AudioMixIdentityTest.kt
@@ -1,6 +1,7 @@
 package com.tajuli.digitorandroid.editor
 
 import com.tajuli.digitorandroid.editor.model.AudioMix
+import com.tajuli.digitorandroid.editor.model.audioNoiseReductionGain
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -11,5 +12,13 @@ class AudioMixIdentityTest {
         assertEquals(1f, mix.volume, 0f)
         assertEquals(0L, mix.fadeInUs)
         assertEquals(0L, mix.fadeOutUs)
+        assertEquals(0f, mix.noiseReduction, 0f)
+    }
+
+    @Test
+    fun noiseReductionLeavesLoudSpeechAndSuppressesLowLevelNoise() {
+        assertEquals(1f, audioNoiseReductionGain(.5f, 1f), 0.0001f)
+        val lowNoiseGain = audioNoiseReductionGain(.005f, 1f)
+        assert(lowNoiseGain < .2f)
     }
 }
